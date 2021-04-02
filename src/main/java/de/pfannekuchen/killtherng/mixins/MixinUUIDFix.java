d7f89ebf653f1da98d78a563550b5ca684253ceb
Pfannekuchen <gamesupport@mgnet.work>
2021-04-02 16:18:41 +0200

    Unbind UUID to System Time Random

---
+++ src/main/java/de/pfannekuchen/killtherng/mixins/MixinUUIDFix.java
@@ -0,0 +1,31 @@
+package de.pfannekuchen.killtherng.mixins;
+
+import java.util.Random;
+import java.util.UUID;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import net.minecraft.entity.Entity;
+
+/**
+ * When an entity gets created it gets a randomly generated UUID, this is bad, because UUID's cannot be the same or the 
+ * Entity won't spawn.
+ * 
+ * @author Pancake
+ */
+@Mixin(Entity.class)
+public class MixinUUIDFix {
+
+	/**
+	 * @author Pancake
+	 * @param rng Parameter from Mixin, this is the actual Random used for this. It's our HijackedRandom, so we ignore it.
+	 * @return Returns an actually random :O UUID.
+	 */
+	@Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/MathHelper;getRandomUUID(Ljava/util/Random;)Ljava/util/UUID;"))
+	public UUID redirectRandom(Random rng) {
+		return UUID.randomUUID();
+	}
+	
+}
